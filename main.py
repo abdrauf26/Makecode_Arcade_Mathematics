41419cd15d07b8b99187aa6107350cc9f49c16b9
Rauf Yusope <96287600+abdrauf26@users.noreply.github.com>
2023-01-12 18:22:27 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 temp_Celsius = 37.6
-game.splash("The temperature is " + ("" + str(temp_Celsius)) + " C")
+game.splash("The temperature is " + str(temp_Celsius) + " C")
 temp_Fahrenheit = temp_Celsius * (9 / 5) + 32
-game.splash("The temperature in Fahrenheit is  " + ("" + str(temp_Fahrenheit)) + " C")
+game.splash("The temperature in Fahrenheit is  " + str(temp_Fahrenheit) + " C")
