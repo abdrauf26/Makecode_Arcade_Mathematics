15ddb0fcc8c291a8e2281085d862ff4c1160297f
Rauf Yusope <96287600+abdrauf26@users.noreply.github.com>
2023-01-12 10:31:08 +0800

    Update main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,2 +1,4 @@
-answer = 3 + 4 * 7
-game.splash("Hello" + str(answer))
+temp_Celsius = 37.6
+game.splash("The temperature is " + ("" + str(temp_Celsius)) + " C")
+temp_Fahrenheit = temp_Celsius * (9 / 5) + 32
+game.splash("The temperature in Fahrenheit is  " + ("" + str(temp_Fahrenheit)) + " C")
--- main.ts
+++ main.ts
@@ -1,2 +1,4 @@
-let answer = 3 + 4 * 7
-game.splash("3 + 4 x 7" + ("" + answer))
+let temp_Celsius = 37.6
+game.splash("The temperature is " + temp_Celsius + " C")
+let temp_Fahrenheit = temp_Celsius * (9 / 5) + 32
+game.splash("The temperature in Fahrenheit is  " + temp_Fahrenheit + " C")
